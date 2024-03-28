418f2eee277187233796012ea2a70d0282ce9942
Marco Di Bella <mdb@marcodibella.de>
2024-03-28 19:00:40 +0100

    Add cache busting

--- includes/block-styles.php
+++ includes/block-styles.php
@@ -22,16 +22,24 @@ defined( 'ABSPATH' ) or exit;
  */
 
 function register_block_styles() {
-    wp_enqueue_script(
-        'ph_THEME-PREFIX-block-styles',
-        THEME_URI . 'assets/src/js/block-styles.js',    // maybe add a 'build' version?
-        [
-            'wp-blocks',
-            'wp-dom-ready',
-            'wp-edit-post'
-        ],
-        THEME_VERSION
-    );
+
+    $filename = 'assets/src/js/block-styles.js';
+    //$filename = 'assets/build/js/block-styles.min.js';
+
+    if( file_exists( THEME_DIR . $filename ) ) {
+
+        wp_enqueue_style(
+            'artlyris-block-style',
+            THEME_URI . $filename,
+            [
+                'wp-blocks',
+                'wp-dom-ready',
+                'wp-edit-post'
+            ],
+            THEME_VERSION . '.' . filemtime( THEME_DIR . $filename ),
+        );
+    }
+
 }
 
 add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\register_block_styles' );
