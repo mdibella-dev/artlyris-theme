1cd84d8ac0db89f9d6e26cb3a711751e07296503
Marco Di Bella <mdb@marcodibella.de>
2024-04-02 20:54:30 +0200

    Update archive-title.php

--- patterns/archive-title.php
+++ patterns/archive-title.php
@@ -7,12 +7,20 @@
 ?>
 
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+<!-- wp:group {"align":"full","backgroundColor":"pale-dark-gray","layout":{"type":"constrained"},"spacing":{"padding":{"left":"style|root|padding|left","right":"style|root|padding|right"}}} -->
+<div class="wp-block-group alignfull has-pale-dark-gray-background-color has-background" style="padding-left:var(--wp--style--root--padding-left);padding-right:var(--wp--style--root--padding-right);height:150px;display:flex;flex-wrap:wrap;justify-content:flex-start;align-content:center;align-items:center;">
 
-    <!-- wp:paragraph {"align":"center"} -->
-    <p class="has-text-align-center"><?php echo __( 'You', 'artlyris' ); ?></p>
+    <!-- wp:paragraph {"style":{"spacing":{"margin":"0","padding":"0"}}} -->
+    <p style="padding:0;margin:0;width:100%;"><em>Stichwort</em></p>
     <!-- /wp:paragraph -->
+
+    <!-- wp:group {"spacing":{"margin":{"top":"0"}}} -->
+    <div class="wp-block-group" style="width:100%;margin-top:0;">
+
+        <!-- wp:query-title {"type":"archive","textAlign":"left","showPrefix":false,"style":{"typography":{"fontStyle":"normal","fontWeight":"100","textTransform":"uppercase"},"spacing":{"padding":"0"}},"fontSize":"large","fontFamily":"open-sans"} /-->
+
+    </div>
+    <!-- /wp:group -->
 
 </div>
 <!-- /wp:group -->
