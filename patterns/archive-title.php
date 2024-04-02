033109a7023ac2e6564c163038ed0d2ac03c041e
Marco Di Bella <mdb@marcodibella.de>
2024-04-02 07:36:01 +0200

    Add archive/tag template/pattern

---
+++ patterns/archive-title.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Archive Title
+ * Slug: artlyris-theme/archive-title
+ * Categories: archive
+ */
+?>
+
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+
+    <!-- wp:paragraph {"align":"center"} -->
+    <p class="has-text-align-center"><?php echo __( 'You', 'artlyris' ); ?></p>
+    <!-- /wp:paragraph -->
+
+</div>
+<!-- /wp:group -->
