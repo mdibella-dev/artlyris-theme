444b17cc944eeda31a4cab35725a6abd970a7432
Marco Di Bella <mdb@marcodibella.de>
2024-04-04 21:04:25 +0200

    Update posts-3-col.php

--- patterns/posts-3-col.php
+++ patterns/posts-3-col.php
@@ -10,10 +10,10 @@
 <!-- wp:query {"query":{"perPage":12,"pages":0,"offset":"0","postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true}","layout":{"type":"default"}} -->
 <div class="wp-block-query">
 
-    <!-- wp:post-template {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"grid","columnCount":3}} -->
+    <!-- wp:post-template {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"grid","columnCount":3}} -->
 
         <!-- wp:post-featured-image {"isLink":true,"aspectRatio":"4/3"} /-->
-        <!-- wp:post-title {"isLink":true,"style":{"layout":{"flexSize":"min(2.5rem, 3vw)","selfStretch":"fixed"},"typography":{"fontStyle":"normal","fontWeight":"300","textTransform":"uppercase"},"fontSize":"large"}} /-->
+        <!-- wp:post-title {"isLink":true,"style":{"typography":{"fontStyle":"normal","fontWeight":"400","textTransform":"uppercase"}},"fontSize":"medium","fontFamily":"open-sans"} /-->
 
     <!-- /wp:post-template -->
 
