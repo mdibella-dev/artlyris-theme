4286ecf984270f8f6fa01f13c664fc73280fac12
Marco Di Bella <mdb@marcodibella.de>
2024-04-07 10:37:51 +0200

    Add 4 column teaser view

---
+++ patterns/posts-4-col.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: List of posts, 3 columns
+ * Slug: artlyris-theme/posts-4-col
+ * Categories: query
+ * Block Types: core/query
+ */
+?>
+
+<!-- wp:query {"query":{"perPage":12,"pages":0,"offset":"0","postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true}","layout":{"type":"default"}} -->
+<div class="wp-block-query">
+
+    <!-- wp:post-template {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"grid","columnCount":4}} -->
+
+        <!-- wp:post-featured-image {"isLink":true,"aspectRatio":"4/3"} /-->
+        <!-- wp:post-title {"isLink":true,"style":{"typography":{"fontStyle":"normal","fontWeight":"400","textTransform":"uppercase"}},"fontSize":"medium","fontFamily":"open-sans"} /-->
+
+    <!-- /wp:post-template -->
+
+</div>
+<!-- /wp:query -->
