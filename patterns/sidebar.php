7eb8798933ef1a00f4426abf9a3a82ccc29dacf6
Marco Di Bella <mdb@marcodibella.de>
2024-04-20 21:03:30 +0200

    Add link to the corresponding edition page in the sidebar

--- patterns/sidebar.php
+++ patterns/sidebar.php
@@ -37,5 +37,47 @@
     }
     ?>
 
+    <?php
+    if ( has_term( '', 'edition') ) {
+
+        global $post;
+        $pages = [];
+        $x;
+
+        $terms = get_the_terms( $post, 'edition' );
+
+        if ( is_array( $terms ) ) {
+            $x = $terms[0]->term_id;
+            $pages = get_posts( [
+                'post_type'   => 'page',
+                'numberposts' => 1,
+                'tax_query'   => [
+                    [
+                        'taxonomy'         => 'edition',
+                        'field'            => 'term_id',
+                        'terms'            => $terms[0]->term_id,
+                        'include_children' => false
+                    ]
+                ]
+            ] );
+        }
+
+        if( ! empty( $pages ) ) {
+    ?>
+    <!-- wp:group {"className":"site-component-sidebar-tool"} -->
+    <div class="wp-block-group site-component-sidebar-tool">
+
+        <!-- wp:heading {"level":6,"className":"is-style-sidebar-tool"} /-->
+        <h6 class="wp-block-heading is-style-sidebar-tool"><span><?php echo __( 'Editions', 'artlyris' ); ?></span></h6>
+
+        <p><a href="<?php echo get_permalink( $pages[0] ); ?>"><?php echo __( 'Part of this edition', 'artlyris' ); ?></a></p>
+
+    </div>
+    <!-- /wp:group -->
+    <?php
+        }
+    }
+    ?>
+
 </aside>
 <!-- /wp:group -->
