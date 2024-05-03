5d36e1b24b6304451382355669a22be2e964f778
Marco Di Bella <mdb@marcodibella.de>
2024-05-03 18:45:17 +0200

    Add author info

---
+++ patterns/single-author-info.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Title: Single Author Info
+ * Slug: artlyris-theme/single-author-info
+ * Categories: post
+ */
+?>
+
+
+<!-- wp:group {"align":"full","layout":{"type":"constrained"},"className":"site-component-author-info"} -->
+<div class="wp-block-group alignfull site-component-author-info">
+
+    <!-- wp:columns -->
+    <div class="wp-block-columns">
+
+        <!-- wp:column {"width":"96px"} -->
+        <div class="wp-block-column" style="flex-basis:96px">
+
+            <!-- wp:avatar /-->
+
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column -->
+        <div class="wp-block-column">
+
+            <!-- wp:post-author-name {"isLink":true } /-->
+            <!-- wp:post-author-biography /-->
+
+        </div>
+        <!-- /wp:column -->
+
+    </div>
+    <!-- /wp:columns -->
+
+</div>
+<!-- /wp:group -->
