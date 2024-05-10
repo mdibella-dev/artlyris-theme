77f7885152fd06452a9ba13f67781e089f806503
Marco Di Bella <mdb@marcodibella.de>
2024-05-10 17:03:34 +0200

    Update posts-4-col.php

--- patterns/posts-4-col.php
+++ patterns/posts-4-col.php
@@ -22,7 +22,7 @@
     <!-- wp:post-template {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"grid","columnCount":4}} -->
 
         <!-- wp:post-featured-image {"isLink":true,"aspectRatio":"4/3"} /-->
-        <!-- wp:post-title {"isLink":true,"style":{"typography":{"fontStyle":"normal","fontWeight":"400","textTransform":"uppercase"},"spacing":{"margin":{"bottom":"0"}}},"fontSize":"medium","fontFamily":"open-sans"} /-->
+        <!-- wp:post-title {"isLink":true,"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"400","textTransform":"uppercase"},"spacing":{"margin":{"bottom":"0"}}},"fontSize":"small","fontFamily":"open-sans"} /-->
 
     <!-- /wp:post-template -->
 
