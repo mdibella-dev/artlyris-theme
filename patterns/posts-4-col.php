40263192ae1eda3b767dcb6ca117e4b60846cdc7
Marco Di Bella <mdb@marcodibella.de>
2024-05-16 09:53:41 +0200

    Update posts-4-col.php

--- patterns/posts-4-col.php
+++ patterns/posts-4-col.php
@@ -10,7 +10,7 @@
 <!-- wp:query {"query":{"perPage":12,"pages":0,"offset":"0","postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true}","layout":{"type":"default"}} -->
 <div class="wp-block-query">
 
-    <!-- wp:query-pagination {"paginationArrow":"arrow","layout":{"type":"flex","justifyContent":"center"}} -->
+    <!-- wp:query-pagination {"paginationArrow":"arrow","layout":{"type":"flex","justifyContent":"center"},"style":{"typography":{"fontStyle":"normal","fontWeight":"400","textTransform":"uppercase"}},"fontSize":"small","fontFamily":"open-sans"} -->
         <!-- wp:query-pagination-previous /-->
         <!-- wp:query-pagination-next /-->
     <!-- /wp:query-pagination -->
@@ -21,7 +21,7 @@
 
     <!-- wp:post-template {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"grid","columnCount":4}} -->
 
-        <!-- wp:post-featured-image {"isLink":true,"aspectRatio":"4/3"} /-->
+        <!-- wp:post-featured-image {"isLink":true,"aspectRatio":"4/3","style":{"border":{"radius":"3px"}}} /-->
         <!-- wp:post-title {"isLink":true,"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"400","textTransform":"uppercase"},"spacing":{"margin":{"bottom":"0"}}},"fontSize":"small","fontFamily":"open-sans"} /-->
         <!-- wp:pattern {"slug":"artlyris-theme/post-category-terms"} /-->
 
@@ -31,7 +31,7 @@
     <div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
     <!-- /wp:spacer -->
 
-    <!-- wp:query-pagination {"paginationArrow":"arrow","layout":{"type":"flex","justifyContent":"center"}} -->
+    <!-- wp:query-pagination {"paginationArrow":"arrow","layout":{"type":"flex","justifyContent":"center"},"style":{"typography":{"fontStyle":"normal","fontWeight":"400","textTransform":"uppercase"}},"fontSize":"small","fontFamily":"open-sans"} -->
         <!-- wp:query-pagination-previous /-->
         <!-- wp:query-pagination-next /-->
     <!-- /wp:query-pagination -->
