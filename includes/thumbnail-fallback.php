8452e472be46f5e631c56fe02dda62491cad865b
Marco Di Bella <mdb@marcodibella.de>
2024-05-25 14:51:14 +0200

    Create thumbnail-fallback.php

---
+++ includes/thumbnail-fallback.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Function to return a fallback image (a placeholder) for posts without a thumbnail.
+ *
+ * @author  Marco Di Bella
+ * @package ARTlyris
+ */
+
+namespace artlyris;
+
+
+/** Prevent direct access */
+
+defined( 'ABSPATH' ) or exit;
+
+
+
+/**
+ * Returns a HTML code for the fallback image if no thumbnail is present.
+ *
+ * @since 1.0.0
+ * @see   https://developer.wordpress.org/reference/hooks/post_thumbnail_html/
+ */
+
+function post_thumbnail_fallback_html( $html, $post_id, $post_thumbnail_id, $size, $attr ) {
+
+    if ( empty( $html ) ) {
+
+    $html = "x";
+
+    };
+
+    return $html;
+}
+
+
+add_filter( 'post_thumbnail_html', __NAMESPACE__ . '\post_thumbnail_fallback_html', 20, 5 );
