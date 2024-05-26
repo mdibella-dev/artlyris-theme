e49f91e8eb8666f304e68af71d35cc3369f62e1e
Marco Di Bella <mdb@marcodibella.de>
2024-05-26 15:59:38 +0200

    Update post-category-terms.php

--- patterns/post-category-terms.php
+++ patterns/post-category-terms.php
@@ -8,8 +8,10 @@
 ?>
 
 
-<!-- wp:separator {"style":{"spacing":{"margin":{"top":"1.5rem","bottom":".5rem"}},"border":{"top":{"width":"1px"}}},"backgroundColor":"dark-blue","className":"is-style-default"} -->
-<hr class="wp-block-separator has-text-color has-dark-blue-color has-alpha-channel-opacity has-dark-blue-background-color has-background is-style-default" style="border-top-width:1px;margin-top:1.5rem;margin-bottom:.5rem"/>
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"1.5rem","bottom":".25rem"}},"border":{"top":{"width":"1px"}}},"backgroundColor":"dark-blue","className":"is-style-default"} -->
+<hr class="wp-block-separator has-text-color has-dark-blue-color has-alpha-channel-opacity has-dark-blue-background-color has-background is-style-default" style="border-top-width:1px;margin-top:1.5rem;margin-bottom:.25rem"/>
 <!-- /wp:separator -->
 
-<!-- wp:post-terms {"term":"category","fontSize":"small","fontFamily":"playfair","style":{"typography":{"fontStyle":"italic"},"elements":{"link":{"color":{"text":"var:preset|color|dark-blue"}}}}} /-->
+
+
+<!-- wp:post-terms {"term":"category","fontSize":"small","fontFamily":"playfair","style":{"spacing":{"margin":{"blockStart":"0"}},"typography":{"fontStyle":"italic"},"elements":{"link":{"color":{"text":"var:preset|color|dark-blue"}}}}} /-->
