8ca7d34ec5b8d9f259a03d1ee0be6c458822e48e
Marco Di Bella <mdb@marcodibella.de>
2024-05-31 14:47:43 +0200

    Rename authors list template, add editions list template

--- patterns/header-navigation.php
+++ patterns/header-navigation.php
@@ -32,7 +32,7 @@
 
         <!-- wp:list-item {"className":"site-component-header-navigation__desktop-item"} -->
         <li class="site-component-header-navigation__desktop-item">
-            <a class="site-component-header-navigation__desktop-link" href="#"><?php echo __( 'Editions', 'artlyris' ); ?></a>
+            <a class="site-component-header-navigation__desktop-link" href="/editionen/"><?php echo __( 'Editions', 'artlyris' ); ?></a>
         </li>
         <!-- /wp:list-item -->
 
