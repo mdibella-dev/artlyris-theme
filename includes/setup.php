0c927fefa47ecca5a151eea9ac7dd51d81133cdb
Marco Di Bella <m_dibella@icloud.com>
2024-08-04 21:01:04 +0200

    Update setup.php

--- includes/setup.php
+++ includes/setup.php
@@ -47,115 +47,96 @@ add_action( 'after_setup_theme', __NAMESPACE__ . '\theme_setup' );
 
 function theme_scripts() {
 
-    /**
-     * Registers and loads the theme's own scripts.
-     */
-
-    $scripts = [
-        'anime' => [
-            'path'    => 'assets/build/vendors/anime/anime.min.js',
-            'deps'    => [],
-            'version' => false,
-            'args'    => [
-                'in_footer' => true,
-                'strategy'  => 'async'
-            ]
-        ],
-        'artlyris-frontend-script' => [
-            'path'    => /*'assets/build/js/frontend.min.js'*/ 'assets/src/js/frontend.js',
-            'deps'    => [
-                'anime'
+    $collection = [
+        'scripts' => [
+            'anime' => [
+                'path'    => 'assets/build/vendors/anime/anime.min.js',
+                'deps'    => [],
+                'version' => false,
+                'args'    => [
+                    'in_footer' => true,
+                    'strategy'  => 'async'
+                ]
             ],
-            'version' => '',
-            'args'    => [
-                'in_footer' => true,
-                'strategy'  => 'async'
+            'artlyris-frontend-script' => [
+                'path'    => 'assets/build/js/frontend.min.js',
+                'deps'    => [
+                    'anime'
+                ],
+                'version' => '',
+                'args'    => [
+                    'in_footer' => true,
+                    'strategy'  => 'async'
+                ]
+            ],
+            'artlyris-custom-cursor-script' => [
+                'path'    => 'assets/build/js/custom-cursor.min.js',
+                'deps'    => [],
+                'version' => '',
+                'args'    => [
+                    'in_footer' => true,
+                    'strategy'  => 'async'
+                ]
             ]
         ],
-        'artlyris-custom-cursor-script' => [
-            'path'    => /*'assets/build/js/custom-cursor.min.js'*/ 'assets/src/js/custom-cursor.js',
-            'deps'    => [],
-            'version' => '',
-            'args'    => [
-                'in_footer' => true,
-                'strategy'  => 'async'
+        'styles' => [
+            'artlyris-frontend-style' => [
+                'path'    => 'assets/build/css/frontend.min.css',
+                'deps'    => [],
+                'version' => ''
+            ],
+            'artlyris-custom-cursor-style' => [
+                'path'    => 'assets/build/css/custom-cursor.min.css',
+                'deps'    => [],
+                'version' => ''
             ]
         ]
     ];
 
-    // Register (and enqueue) all scripts in the given order
-    foreach ( $scripts as $handle => $setup ) {
+    foreach ( $collection as $category => $files ) {
 
-        if ( file_exists( THEME_DIR . $setup['path'] ) ) {
+        // Register (and enqueue) all scripts/styles in the given order
+        foreach ( $files as $handle => $setup ) {
 
-            $version = '';
+            if ( file_exists( THEME_DIR . $setup['path'] ) ) {
 
-            if ( is_string( $setup['version'] ) ) {
-                $version = ( empty( $setup['version'] ) )? THEME_VERSION . '.' . filemtime( THEME_DIR . $setup['path'] ) : $setup['version'];
-            }  else if ( is_bool( $setup['version'] ) and ( false === $setup['version'] ) ) {
-                $version = false;
-            } else {
-                $version = null;
+                $version = '';
+
+                if ( is_string( $setup['version'] ) ) {
+                    $version = ( empty( $setup['version'] ) )? THEME_VERSION . '.' . filemtime( THEME_DIR . $setup['path'] ) : $setup['version'];
+                }  else if ( is_bool( $setup['version'] ) and ( false === $setup['version'] ) ) {
+                    $version = false;
+                } else {
+                    $version = null;
+                }
+
+                if ( 'scripts' === $category ) {
+
+                    wp_register_script(
+                        $handle,
+                        THEME_URI . $setup['path'],
+                        $setup['deps'],
+                        $version,
+                        $setup['args'],
+                    );
+
+                    wp_enqueue_script( $handle );
+
+                } else if ( 'styles' === $category ) {
+
+                    wp_register_style(
+                        $handle,
+                        THEME_URI . $setup['path'],
+                        $setup['deps'],
+                        $version
+                    );
+
+                    wp_enqueue_style( $handle );
+
+                } else {
+                    // do nothing
+                }
             }
-
-            wp_register_script(
-                $handle,
-                THEME_URI . $setup['path'],
-                $setup['deps'],
-                $version,
-                $setup['args'],
-            );
-
-            wp_enqueue_script( $handle );
-        }
-
-    }
-
-
-    /**
-     * Registers and loads the theme's own styles.
-     *
-     * Note: The style.css in the main directory is only used for theme identification and versioning.
-     * Actually the (compressed) style information can be found in frontend-style(.min).css.
-     */
-
-    $styles = [
-        'artlyris-frontend-style' => [
-            'path'    => 'assets/build/css/frontend.min.css',
-            'deps'    => [],
-            'version' => ''
-        ],
-        'artlyris-custom-cursor-style' => [
-            'path'    => 'assets/build/css/custom-cursor.min.css',
-            'deps'    => [],
-            'version' => ''
-        ]
-    ];
-
-
-    // Register (and enqueue) all styles in the given order
-    foreach ( $styles as $handle => $setup ) {
-
-        if ( file_exists( THEME_DIR . $setup['path'] ) ) {
-
-            $version = '';
-
-            if ( is_string( $setup['version'] ) ) {
-                $version = ( empty( $setup['version'] ) )? THEME_VERSION . '.' . filemtime( THEME_DIR . $setup['path'] ) : $setup['version'];
-            }  else if ( is_bool( $setup['version'] ) and ( false === $setup['version'] ) ) {
-                $version = false;
-            } else {
-                $version = null;
-            }
-
-            wp_register_style(
-                $handle,
-                THEME_URI . $setup['path'],
-                $setup['deps'],
-                $version
-            );
-
-            wp_enqueue_style( $handle );
         }
 
     }
